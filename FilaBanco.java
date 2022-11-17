c28cff72694da629b3450a9d74e2f1863803336c
isabelasnts <101901724+isabelasnts@users.noreply.github.com>
2022-11-17 12:03:32 -0300

    Update FilaBanco.java

--- FilaBanco.java
+++ FilaBanco.java
@@ -13,17 +13,17 @@ public class Main {
  
     public static void main(String[] args) { 
 	Scanner nome = new Scanner(System.in);
-    String[] nomesFila = new String[3]; 
-    int next = 0;
+    	String[] nomesFila = new String[3]; 
+    	int next = 0;
     
-    for(int i = 0; i < 3; i++) {
-    	String N = nome.next();
-    	nomesFila[next] = N;
-    	next++;
-    }
+    	for(int i = 0; i < 3; i++) {
+    		String N = nome.next();
+    		nomesFila[next] = N;
+    		next++;
+   	}
     
-    for(int i = 0; i < 3; i++) {
-    	System.out.println (nomesFila[i] + " - esta na posicao: " + (i+1));
+    	for(int i = 0; i < 3; i++) {
+    		System.out.println (nomesFila[i] + " - esta na posicao: " + (i+1));
+   	}
     } 
-  } 
 }
